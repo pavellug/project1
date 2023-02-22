aeb84adc8879338aa96a712ad29170d955c68600
20399 <20399@1k129s14.dune.edu>
2023-02-22 10:36:53 +0200

    Добавление файла2

--- src/lb4/Lb4.java
+++ src/lb4/Lb4.java
@@ -16,7 +16,7 @@ public class Lb4 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Second commit");
+       
 
         }
        
