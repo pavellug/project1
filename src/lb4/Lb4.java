8a5f4eee67add0ff6c22167d43568c3412d69bd5
20399 <20399@1k129s14.dune.edu>
2023-02-22 11:14:23 +0200

    commit 2

--- src/lb4/Lb4.java
+++ src/lb4/Lb4.java
@@ -16,7 +16,8 @@ public class Lb4 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
-        
+        System.out.println("Hello 1");
+        System.out.println("Hello");
        
 
         }
