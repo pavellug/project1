3167059756fc7dc9241e3bee3aadbee1f723826a
20397 <20397@1k129s13.dune.edu>
2023-02-22 10:22:06 +0200

    commit 2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -19,6 +19,7 @@ public class JavaApplication1 {
         for(int i = 0; i < 10; i++) {
             
             System.out.println("Hello world");
+            System.out.println("world");
         
         }
     }
