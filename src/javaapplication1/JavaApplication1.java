f5f45f827cd0842a257e4e9badec0b9adad2f276
20397 <20397@1k129s13.dune.edu>
2023-02-22 10:21:37 +0200

    commit 1

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author 20397
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++) {
+            
+            System.out.println("Hello world");
+        
+        }
+    }
+    
+}
