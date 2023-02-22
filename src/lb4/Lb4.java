418248149962f3413fe3a72ef67da34772fe529a
20399 <20399@1k129s14.dune.edu>
2023-02-22 10:53:08 +0200

    Обновление ветки

--- src/lb4/Lb4.java
+++ src/lb4/Lb4.java
@@ -16,6 +16,9 @@ public class Lb4 {
      */
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
        
 
         }
