b487667a8cc5945b37e896beceb6efbef08af71f
pavellug <126480317+pavellug@users.noreply.github.com>
2023-02-27 16:02:27 +0200

    Update JavaApplication1.java

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaapplication1;
 
-/**
- *
- * @author 20397
- */
 public class JavaApplication1 {
 
     /**
