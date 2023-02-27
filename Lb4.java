75c4d9223a8184a195a271ddc29f220548ddad95
pavellug <126480317+pavellug@users.noreply.github.com>
2023-02-27 15:29:40 +0200

    Update Lb4.java

--- Lb4.java
+++ Lb4.java
@@ -6,6 +6,7 @@
 package lb4;
 
 /**
+
  *
  * @author 20399
  */
@@ -17,7 +18,7 @@ public class Lb4 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello 1");
-        System.out.println("Hello");
+      
        
 
         }
